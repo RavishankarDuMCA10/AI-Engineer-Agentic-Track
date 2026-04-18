50ba5cf6cc0651751eaa5ccaf1d3188b9daa6c19
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2026-04-18 21:34:31 +0530

    Added classes of Price related classes and DL solution.

--- Section6_TraditionalML_To_DL_Fine_Tuning/pricer/items.py
+++ Section6_TraditionalML_To_DL_Fine_Tuning/pricer/items.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from datasets import Dataset, DatasetDict, load_dataset
+from datasets import Dataset, DatasetDict, load_dataset  # type: ignore[import]
 from typing import Optional, Self
 
 PREFIX = "Price is $"
@@ -24,13 +24,32 @@ class Item(BaseModel):
         self.prompt = f"{QUESTION}\n\n{text}\n\n{PREFIX}{round(self.price)}.00"
 
     def test_prompt(self) -> str:
+        if self.prompt is None:
+            raise ValueError("Prompt is not set")
         return self.prompt.split(PREFIX)[0] + PREFIX
 
     def __repr__(self) -> str:
         return f"<{self.title} = ${self.price}>"
 
-    @staticmethod
+    @classmethod
     def push_to_hub(
-        dataset_name: str, train: list[Self], val: list[Self], test: list[Self]
+        cls, dataset_name: str, train: list[Self], val: list[Self], test: list[Self]
     ):
-        """Push Item listd to HuggingFace Hub"""
+        """Push Item lists to HuggingFace Hub"""
+        DatasetDict(
+            {
+                "train": Dataset.from_list([item.model_dump() for item in train]),
+                "validation": Dataset.from_list([item.model_dump() for item in val]),
+                "test": Dataset.from_list([item.model_dump() for item in test]),
+            }
+        ).push_to_hub(dataset_name)
+
+    @classmethod
+    def from_hub(cls, dataset_name: str) -> tuple[list[Self], list[Self], list[Self]]:
+        """Load from Huggingface Hub and reconstruct Items"""
+        ds = load_dataset(dataset_name)
+        return (
+            [cls.model_validate(row) for row in ds["train"]],
+            [cls.model_validate(row) for row in ds["validation"]],
+            [cls.model_validate(row) for row in ds["test"]],
+        )
